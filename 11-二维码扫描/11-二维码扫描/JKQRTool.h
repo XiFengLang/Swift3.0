00d95977adc35fe603108a3f13e8d19e11d793b1
溪枫狼 <lang131jp@vip.qq.com>
2016-12-14 09:36:10 +0800

    没啥东西
    没啥东西

--- 11-二维码扫描/11-二维码扫描/JKQRTool.h
+++ 11-二维码扫描/11-二维码扫描/JKQRTool.h
@@ -10,6 +10,26 @@
 #import <UIKit/UIKit.h>
 
 @interface JKQRTool : NSObject
+
+
+/**
+ 白色透明，黑色变色
+
+ @param image 原图
+ @param red r
+ @param green g
+ @param blue b
+ @return 彩色图
+ */
 - (UIImage*)imageBlackToTransparent:(UIImage*)image withRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue;
+
+
+
+/**
+ 生成条形码
+
+ @param barCode 内容
+ @return 条形码
+ */
 + (UIImage *)generateBarCode:(NSString *)barCode;
 @end
